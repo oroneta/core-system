101944b219bc40e3277bd76291635777ed4970d4
tostadanevera <mitostadoraquemada@gmail.com>
2024-03-24 21:15:04 +0100

    DB connection function added

--- src/c/base_DB.cpp
+++ src/c/base_DB.cpp
@@ -1,4 +1,25 @@
-// Function que conecte ddbb
-// https://www.youtube.com/watch?v=Y04aiv4opoA
+#include <iostream>
+#include <cstdlib>
+#include <pqxx/pqxx>
+
+using namespace std;
+
+void DB_connection(const string& ip, int port, const string& user, const string& password) {
+    try {
+        string conn_str = "dbname = " +  getenv(PG_DBNAME) +  " user = " + user + 
+                                " password = " + password + " hostaddr = " + ip + 
+                                " port = " + to_string(port);
+        connection c(conn_str);
+        
+        if (c.is_open()) {
+            cout << "Opened database successfully" << endl;
+        } else {
+            cout << "Can't open database" << endl;
+        }
+    } catch (const std::exception &e) {
+        cerr << e.what() << endl;
+    }
+}
+
 
 
