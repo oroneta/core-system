bb7f68052cd7387b88d72613e6434896805129d1
tostadanevera <mitostadoraquemada@gmail.com>
2024-04-24 22:51:50 +0200

    All functions done

--- src/c/base_DB.cpp
+++ src/c/base_DB.cpp
@@ -53,3 +53,67 @@ bool checkDic(PGconn* conn, char* dic) {
     }
     return false;
 }
+
+// insert route
+void insertRoute(PGconn* conn, char* dic, char* auth_code, char* route) {
+    if (PQstatus(conn) == CONNECTION_OK) {
+        char query[256];
+        snprintf(query, sizeof(query), "INSERT INTO routes (dic, auth_code, flight) VALUES ('%s', '%s', '%s')", dic, auth_code, route);
+
+        if (PQresultStatus(PQexec(conn, query)) != PGRES_COMMAND_OK) {
+            printf("Error executing query: %s\n", PQerrorMessage(conn));
+        }
+    }
+}
+
+// check if dic and auth-code match
+bool checkAuthCode(PGconn* conn, char* dic, char* authCode) {
+    if (PQstatus(conn) == CONNECTION_OK) {
+        char query[256];
+         PGresult* result;
+
+        snprintf(query, sizeof(query), "SELECT EXISTS (SELECT 1 FROM drones WHERE dic = '%s' AND authCode = '%s)", dic, authCode);
+
+        result = PQexec(conn, query);
+
+         if (PQresultStatus(result) != PGRES_TUPLES_OK) {
+            printf("Error executing query: %s\n", PQerrorMessage(conn));
+            PQclear(result);
+        }
+
+        if (strcmp(PQgetvalue(result, 0, 0), "t") == 0) {
+            printf("Coincide\n");
+            PQclear(result);
+            return true;
+        }
+        printf("No coincide\n");
+        
+    }
+   return true;
+}
+
+// check colissions route
+int checkColissions(PGconn* conn, char* dic, char* route) {
+    if (PQstatus(conn) == CONNECTION_OK) {
+        char query[256];
+        PGresult* result;
+
+        snprintf(query, sizeof(query), 
+            "SELECT COUNT(*) "
+            "FROM ("
+                "SELECT json_array_elements_text(flight::json) AS flight "
+                "FROM routes"
+            ") sub "
+            "WHERE flight = ANY(ARRAY(SELECT json_array_elements_text('%s')));", route);
+
+        result = PQexec(conn, query);
+
+        if (PQresultStatus(result) != PGRES_TUPLES_OK) {
+            printf("Error executing query: %s\n", PQerrorMessage(conn));
+            PQclear(result);
+        }
+        
+        int count = atoi(PQgetvalue(result, 0, 0));
+        return count;
+    }
+}
